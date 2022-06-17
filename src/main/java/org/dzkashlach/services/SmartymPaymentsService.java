8ec34ccb38b6ba61c844fa662cfd760e03012c4e
Dzmitry_Kashlach <Bear doesn't like honey>
2022-06-17 15:46:58 +0300

    accessToken

--- src/main/java/org/dzkashlach/services/SmartymPaymentsService.java
+++ src/main/java/org/dzkashlach/services/SmartymPaymentsService.java
@@ -1,13 +1,21 @@
 package org.dzkashlach.services;
 
+import okhttp3.ResponseBody;
 import org.dzkashlach.entities.PaymentRequest;
+import org.springframework.web.bind.annotation.RequestParam;
 import retrofit2.Call;
 import retrofit2.http.Body;
+import retrofit2.http.Header;
 import retrofit2.http.POST;
-
-import java.util.Map;
+import retrofit2.http.Query;
 
 public interface SmartymPaymentsService {
     @POST("/payment-requests")
     Call<Void> requestPayment(@Body PaymentRequest paymentRequest);
+
+    @POST("/payment-requests")
+    Call<Void> requestPayment(@Body PaymentRequest paymentRequest, @Header("Authorization") String authorization);
+
+    @POST("/token")
+    Call<ResponseBody> requestToken(@Query("client_id") String clientId, @Query("client_secret") String clientSecret, @Query("code") String code, @Query("scope") String scope);
 }
